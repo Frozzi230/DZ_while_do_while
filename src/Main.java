4169f42ca5c76d6e7d1c84385cac6b86a7f4109b
Frozzi230 <aaserik28011@gmai.com>
2026-03-23 15:46:47 +0400

    Проверка дз

--- src/Main.java
+++ src/Main.java
@@ -36,43 +36,102 @@ public class Main {
 
         System.out.println();
 
-        System.out.println("Задача 2");
+        System.out.println("Задача 3");
         int money = 700;
         int day = 5;
-//        System.out.println("Версия 1");
-//        while (money != 0){
-//            count += 1;
-//            if (count % day == 0){
-//                continue;
-//            }
-//            money = money - 100;
-//            if (money == 0){
-//                break;
-//            }
-//            int leftDay = money / 100;
-//            System.out.println(money + " рублей хватит на " + leftDay + " дней");
-//        }
-
-        System.out.println("Версия 2");
-        for (count = 0; money != 0; count ++){
-            int leftDay = 0;
-            if (count == 1){
-                leftDay = money / 100;
-                System.out.println(money + " рублей хватит на " + leftDay + " дней " + count);
-            }
-            if (count % 5 == 0){
+        int dayUsed = 0;
+        System.out.println("Версия 1");
+        count = 1;
+        while (true){
+            if (count % day == 0){
+                dayUsed ++;
+                count ++;
                 continue;
             }
-            else {
-                money = money - 100;
-                leftDay = money / 100;
-                if (money == 0){
+            else{
+                if (money >= 100){
+                    money -= 100;
+                    dayUsed ++;
+                    count ++;
+                }
+                else{
                     break;
                 }
-                System.out.println(money + " рублей хватит на " + leftDay + " дней");
             }
-
         }
+        System.out.println("Бюджета хватит на " + dayUsed + " дней");
+
+        System.out.println("Версия 2");
+        money = 700;
+        dayUsed = 0;
+        for (count = 1; ; count++) {
+            if (count % day == 0) {
+                dayUsed++;
+                continue;
+            } else {
+                if (money >= 100) {
+                    money -= 100;
+                    dayUsed++;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println("Бюджета хватит на " + dayUsed + " дней");
+
+        System.out.println();
+
+        System.out.println("Задача 4");
+        int month = 0;
+        int total = 0;
+        int end = 12000000;
+        System.out.println("Версия 1");
+        while (true){
+            month++;
+            total += 15000;
+            if (month % 6 == 0){
+                total= total + (total / 100 * 7);
+            }
+            System.out.println("Номер месяца " + month + ", накопленная сумма: " + total);
+            if (total >= end){
+                break;
+            }
+        }
+        System.out.println("Версия 2");
+        month = 0;
+        total = 0;
+        do{
+            month++;
+            total += 15000;
+            if (month % 6 == 0){
+                total= total + (total / 100 * 7);
+            }
+            System.out.println("Номер месяца " + month + ", накопленная сумма: " + total);
+            if (total >= end){
+                break;
+            }
+        }
+        while (true);
+
+        System.out.println();
+
+        System.out.println("Задача 5");
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
+        while (charge != 100){
+            minute++;
+            if (minute % 10 == 0){
+                minute = minute + 2;
+                continue;
+            }
+            charge += 2;
+            if (overheats == 3){
+                System.out.println("Зарядка прекращена. Текущий заряд: " + charge + " %");
+                break;
+            }
+        }
+        System.out.println("Время зарядки составило: " + minute + " минут");
 
     }
 }
